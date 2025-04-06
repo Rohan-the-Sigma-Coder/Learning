33829d52aab24d86438c2f8be1382eea81de4c56
Rohan Jha <rohan-jha@outlook.com>
2025-04-06 14:52:06 -0700

    second commit for 4/6/2025

--- matplotlibLinear.py
+++ matplotlibLinear.py
@@ -1,26 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import re
 alpha_list = ['a', 'b', 'c', 'd', 'e' 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 try:
-    user_input = input('Enter linear equation in the form of (-)#variable (+ or -) #, use spaces: ')
+    user_input = input("Enter a linear equation in the form of '#x + #' (with spaces): ")
     user_input_list = user_input.split()
 except IndexError:
     print('Invalid response')
-coefficient = list(user_input_list[0])
+slope_term = user_input_list[0]
+match = re.match(r"(-?\d+)([a-zA-Z])", slope_term)
 
-if coefficient[0] == '-':
-    if coefficient[2] not in alpha_list:
-        print('Invalid response')
-        quit()
-else:
-    if coefficient[1] not in alpha_list:
-        print('Invalid response')
-        quit()
-y_intercept = user_input_list[1] + user_input_list[2]
-graph_yint = -1 * int(y_intercept[1]) if y_intercept[0] == '-' else int(y_intercept)
-xpoints = np.array([0, 1])
-ypoints = np.array([graph_yint, graph_yint + int(coefficient[0]) if coefficient[0] is not '-' else graph_yint + -1 * int(coefficient[1])])
+if not match or match.group(2) not in alpha_list:
+    print("Invalid format for slope")
+    quit()
+
+slope = int(match.group(1))
+
+# Extract y-intercept
+try:
+    sign = user_input_list[1]
+    intercept = int(user_input_list[2])
+    y_intercept = -intercept if sign == '-' else intercept
+except (IndexError, ValueError):
+    print("Invalid y-intercept format")
+    quit()
+
+# Create x and y points
+xpoints = np.array([0, 10])
+ypoints = slope * xpoints + y_intercept
 plt.plot(xpoints, ypoints)
 timer = 3
 for x in range(3):
