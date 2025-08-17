61c5454f3f2d9ccfc64889f1156cb9f2dd7857e5
Rohan Jha <72093988+Rohan-the-Sigma-Coder@users.noreply.github.com>
2025-08-17 15:29:11 -0700

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,7 +1,7 @@
-# nums = [1, 2, 4, 8, 16]
-# letters = ['a', 'b', 'c', 'd', 'e']
+nums = [1, 2, 4, 8, 16]
+letters = ['a', 'b', 'c', 'd', 'e']
 
-# print(nums.index(8))
+print(nums.index(8))
 
 
 result = []
@@ -9,16 +9,16 @@ result = []
 user_input = input('Enter word seperated only by spaces (no commas, periods, marks, etc.): ')
 test_case_input = 'My name is Rohan'
 
-# word_list = test_case_input.split(' ')
+word_list = test_case_input.split(' ')
 
-# for i in range(len(word_list)):
-#     result.append(word_list[i][-1])
+for i in range(len(word_list)):
+    result.append(word_list[i][-1])
 
 
-# if result == ['y', 'e', 's', 'n']:
-#     print('SUCCESS')
-# else:
-#     print('FAIL')
+if result == ['y', 'e', 's', 'n']:
+    print('SUCCESS')
+else:
+    print('FAIL')
 
 
 
