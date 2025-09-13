ab43c5f40e9b1cb5fc84f19361d0be8bbeb5b795
Rohan Jha <rohan-jha@outlook.com>
2025-09-13 12:00:30 -0700

    rename binary_list.py to binary.py, add numpy_practice.py, pdf_prac.py, and text.txt. Modify playground.py.

---
+++ binary.py
@@ -0,0 +1,38 @@
+def decimal_to_binary(d):
+    binary_list = []
+    while d > 0:
+        r = d % 2
+        d = d//2
+        binary_list.append(r)
+    binary_list = binary_list[::-1]
+    return binary_list
+
+
+
+# d = int(input('Enter a number: '))
+# expected_list = [1, 0, 0]
+# result_list = decimal_to_binary(4)
+# if result_list == expected_list:
+#     print('Program test pass')
+# else:
+#     print("PROGRAM TEST FAIL")
+
+
+
+
+def binary_to_decimal(binary):
+     result_list = []
+     binary_list = list(map(int, str(binary)))
+     for i in range(len(binary_list)):
+         if binary_list[i] == 1:
+             result_list.append(2**i)
+     return sum(result_list)
+
+
+binary = int(input('Binary number: '))
+print(binary_to_decimal(binary))
+         
+
+
+
+
