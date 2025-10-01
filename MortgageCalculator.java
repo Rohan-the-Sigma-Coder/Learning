f1199719697f08d6aa81167f245687be4ee8f17a
Rohan Jha <rohan-jha@outlook.com>
2025-09-30 20:32:02 -0700

    Add CardsSuitType.java and rename mortgage_calculator.java to MortgageCalculator.java, rename fizzbuzz.java to Fizzbuzz.java

---
+++ MortgageCalculator.java
@@ -0,0 +1,49 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class mortgage_calculator {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        
+        Scanner scanner = new Scanner(System.in);
+        int principal = 0;
+        float annualInterest = 0;
+        int years = 0;
+        
+        while (true) {
+            System.out.print("Pricipal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1,000 and 1,000,000.");
+
+        }
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30)
+                break;
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+
+        }
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = scanner.nextInt();
+            if (years >= 1 && years <= 30) 
+                break;
+            System.out.println("Enter a value between 1 and 30");
+
+            
+        }
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+            
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / ((Math.pow(1 + monthlyInterest, numberOfPayments)) - 1);
+        String mortgage_result = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgage_result);
+
+        }
+    }
+
