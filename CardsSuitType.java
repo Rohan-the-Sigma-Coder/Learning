e71f27e11c991a90a9d5b34891331c1459268247
Rohan Jha <rohan-jha@outlook.com>
2025-10-12 21:28:47 -0700

    Add CardsSuitType.class, NumberGuesser.java, NumberGuesser.class. Modify Fizzbuzz.java, Main.java, CardsSuitType.java. Delete javapractice.class

--- CardsSuitType.java
+++ CardsSuitType.java
@@ -5,11 +5,17 @@ public class CardsSuitType {
         this.type = suit;
         this.value = value;
     }
-    public static String type() {
+    public static String getType() {
         return type;
     }
-    public static String value() {
+    public static String getValue() {
         return value;
     }
-}
 
+    public static int add(int num1, int num2) {
+            return num1 + num2;
+    }
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+    }
+}
--- fizzbuzz.java
+++ fizzbuzz.java
@@ -14,5 +14,6 @@ public class Fizzbuzz {
         else
             System.out.println(num);
         }
+        scanner.close()
     }
 
