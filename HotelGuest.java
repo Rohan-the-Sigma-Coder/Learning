5502fe792e04afc6fbf06af12665dd35a0dcf471
Rohan Jha <rohan-jha@outlook.com>
2025-10-20 18:59:37 -0700

    Modify HotelApp.java, HotelGuest.java, HotelRoom.java. Add HotelApp.class, HotelGuest.class, HotelReservations.class, HotelRoom.class

--- HotelGuest.java
+++ HotelGuest.java
@@ -2,7 +2,7 @@ public class HotelGuest {
     private String guestName;
     public long reservationID;
     public HotelGuest (String guestName, long reservationid) {
-        HotelReservations reservationiD = new HotelReservations();
+        HotelReservations reservationiD = new HotelReservations(reservationid, null, null, 0);
         this.reservationID = reservationiD.getReservationID();
         this.guestName = guestName;
     }
